31647727af6fddf21ae5567531a1f4ea6036d9c9
Evgaizen <monkdval@mail.ru>
2020-09-09 09:57:44 +0300

    add basic project structure

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+const morgan = require('morgan')
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.listen(PORT, function(){
+  console.log('Server is running on Port:',PORT);
+});
