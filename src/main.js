a5416019b26319f1914c12452b0830f9867a78dc
Evgaizen <monkdval@mail.ru>
2020-09-09 12:08:29 +0300

    add router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import Home from './pages/home.vue'
+import Scan from './pages/scan.vue'
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'scan',
+    path: '/scan',
+    component: Scan
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes });
+
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
---
+++ src/pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <h1 class="home__title">POMODAPAM</h1>
+        <h2>Нами было просконировано <span>12</span> файлов</h2>
+        <h3>Из них <span>5</span> были заражены</h3>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
