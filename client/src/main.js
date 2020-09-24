3165d87ad479d0514f4d40bf79ff6cd285f45bdf
Evgaizen <monkdval@mail.ru>
2020-09-24 22:33:46 +0300

    add Vuex
    closed #24

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import {router} from './plugins/router'
 
+import store from './store/index'
+
 
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
   }).$mount('#app');
